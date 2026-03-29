19ae434154647a65a7e2ce58b6eabf22361ddb3d
Flávia Gabriella <flaviagabriella.cs@gmail.com>
2026-03-28 23:22:41 -0300

    Autenticando rotas

--- src/router.ts
+++ src/router.ts
@@ -16,30 +16,34 @@ const validarCadastro = [
     body("cpf").notEmpty().withMessage("O cpf é obrigatório")
 ]
 
-router.get("/usuarios", UsuarioController.findAll);
+router.get("/usuarios", AutenticarToken, UsuarioController.findAll);
 router.post("/usuarios", validarCadastro, UsuarioController.create);
 router.get("/usuarios/:id", UsuarioController.getById);
-router.delete("/usuarios/:id", UsuarioController.remove);
-router.put("/usuarios/:id", UsuarioController.update);
+router.delete("/usuarios/:id", AutenticarToken, UsuarioController.remove);
+router.put("/usuarios/:id", AutenticarToken, UsuarioController.update);
 router.post("/entrar", UsuarioController.login);
 
 router.get("/categorias", CategoriaController.findAll);
-router.post("/categorias", CategoriaController.create);
+router.post("/categorias", AutenticarToken, CategoriaController.create);
 router.get("/categorias/:id", CategoriaController.getById);
-router.delete("/categorias/:id", CategoriaController.remove);
-router.put("/categorias/:id", CategoriaController.update);
+router.delete("/categorias/:id", AutenticarToken, CategoriaController.remove);
+router.put("/categorias/:id", AutenticarToken, CategoriaController.update);
 
 
 router.get("/autores", AutorController.findAll);
 router.post("/autores", upload.single("foto"), AutenticarToken, AutorController.create);
 router.get("/autores/:id", AutorController.getById);
-router.delete("/autores/:id", AutorController.remove);
-router.put("/autores/:id", AutorController.update);
+router.delete("/autores/:id", AutenticarToken, AutorController.remove);
+router.put("/autores/:id", AutenticarToken, AutorController.update);
+
 
 router.get("/livros", LivroController.findAll);
-router.post("/livros",upload.fields([{ name: "capa_imagem", maxCount: 1 }, { name: "arquivo_pdf", maxCount: 1 }]),LivroController.create);
+router.post("/livros",upload.fields([{ name: "capa_imagem", maxCount: 1 }, { name: "arquivo_pdf", maxCount: 1 }]), AutenticarToken, LivroController.create);
+router.get("/livros/:id", LivroController.getById);
+router.delete("/livros/:id", AutenticarToken, LivroController.remove);
+router.put("/livros/:id", AutenticarToken, LivroController.update);
 
-router.get("/perfil", AutenticarToken, UsuarioController.perfil)
+
 
 
 
